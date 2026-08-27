3957762275c90c817cd2fcc2272ae45ce08b0e01
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-08-27 15:17:43 +0500

    generated the email and password email to verify the user email and also to check for the request of the forgotten password

---
+++ backend/src/utils/mail.js
@@ -0,0 +1,39 @@
+import Mailgen from 'mailgen';
+
+const emailVerificationMailGenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "WElcome to our App! we are excited to have you on board",
+      action: {
+        instructions: "To verify your email please click on the following button",
+        button: {
+          color: "#22bc66",
+          text: "Verify your email",
+          link: verificationUrl
+        }
+      },
+      outro: "Need help, or have questions? Just reply to this email, we'd love to help."
+    }
+  }
+}
+
+const forgotPasswordMailGenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "we got a request to reset the password of your account",
+      action: {
+        instructions: "To reset your password click on the following button or link",
+        button: {
+          color: "#de1515",
+          text: "Click to reset password",
+          link: passwordResetUrl
+        }
+      },
+      outro: "Need help, or have questions? Just reply to this email, we'd love to help."
+    }
+  }
+}
+
+export { forgotPasswordMailGenContent, emailVerificationMailGenContent };
