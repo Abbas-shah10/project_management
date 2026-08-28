c30f4d19c0b55de3e550558797b5f1204657fe30
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-08-28 16:16:54 +0500

    fix the issue with the sendMail to sendEmail

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import { User } from '../models/user.models.js'
 import { ApiResponse } from '../utils/api-response.js'
 import { ApiError } from '../utils/api-error.js'
 import { asyncHandler } from '../utils/async-handler.js'
-import { emailVerificationMailGenContent, sendMail } from '../utils/mail.js';
+import { emailVerificationMailGenContent, sendEmail } from '../utils/mail.js';
 
 const accessTokenAndRefreshToken = async (userId) => {
   try {
@@ -42,7 +42,7 @@ const registerUser = asyncHandler(async (req, res) => {
   user.emailVerificationExpiry = tokenExpiry;
 
   await user.save({ validateBeforeSave: false })
-  await sendMail({
+  await sendEMail({
     email: user?.email,
     subject: "Please verify your email",
     mailgenContent: emailVerificationMailGenContent(user.username, `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`)
