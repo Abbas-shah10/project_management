95afda4bd0f8755ca46c216b0d5750df60e92d69
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-08-30 21:25:05 +0500

    create userLoginValidator

--- backend/src/validators/index.js
+++ backend/src/validators/index.js
@@ -9,6 +9,12 @@ const userRegisterValidator = () => {
   ]
 }
 
+const userLoginValidator = () => {
+  return [
+    body("email").trim().notEmpty().withMessage("Email is required").isEmail("Email is Invalid"),
+    body("password").trim().notEmpty().withMessage("Password is required")
+  ]
+}
 
 
-export { userRegisterValidator };
+export { userRegisterValidator, userLoginValidator }; 
