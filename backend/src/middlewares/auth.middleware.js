9f3b90e26901cb4f73b69ddf938a5be6b0f2f453
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-08-30 21:25:43 +0500

    create the middleware to verify the JWT

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { User } from '../models/user.models.js';
+import { asyncHandler } from '../utils/async-handler.js';
+import { ApiError } from '../utils/api-error.js';
+import jwt from 'jsonwebtoken'
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token = req.cookies?.refreshToken || req.header("Authorization").replace("Bearer ", "")
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized Request")
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry")
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token")
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid Access Token")
+  }
+})
+
+export { verifyJWT }
