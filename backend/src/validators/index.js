a99e5b7e8ebc1130ab68eeee3e34a0ba97ba384f
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-09-02 15:05:13 +0500

    added all the validators for each of the functionality

--- backend/src/validators/index.js
+++ backend/src/validators/index.js
@@ -16,5 +16,30 @@ const userLoginValidator = () => {
   ]
 }
 
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword").notEmpty().withMessage("Old password is required"),
+    body("newPassword").notEmpty().withMessage("New Password is required")
+  ]
+}
 
-export { userRegisterValidator, userLoginValidator }; 
+const userForgotPasswordValidator = () => {
+  return [
+    body("email").notEmpty().withMessage("Email is required").isEmail().withMessage("Email is invalid"),
+  ]
+}
+
+const userResetForgotPassword = () => {
+  return [
+    body("newPassword").notEmpty().withMessage("Password is required")
+  ]
+}
+
+
+export {
+  userRegisterValidator,
+  userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetForgotPassword,
+}; 
