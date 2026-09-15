4e34f37a45cf8650625c8018edf8baef18d7495b
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-09-15 16:12:09 +0500

    feat: implement ProtectedRoutes component for route protection and update appRoutes to include it

--- frontend/src/routes/appRoutes.tsx
+++ frontend/src/routes/appRoutes.tsx
@@ -3,16 +3,20 @@ import Home from "../pages/Home";
 import AppLayout from "../components/layout/AppLayout";
 import Projects from "../pages/projects/Projects";
 import Login from "../pages/auth/Login";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Auth Routes */}
       <Route path="/login" element={<Login />} />
-      {/* App Layout */}
-      <Route element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
+
+      {/* Protected Routes */}
+      <Route element={<ProtectedRoutes />}>
+        <Route element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Route>
       </Route>
     </Routes>
   );
