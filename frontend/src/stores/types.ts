7a8522d960cca9e3e22e0719d73696bf954261f4
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-09-16 14:32:53 +0500

    deleted the app-store and types.ts files to start fresh store with authentication

--- frontend/src/stores/types.ts
+++
@@ -1,9 +0,0 @@
-export interface FilterState {
-  assignees: string[];
-  priorities: string[];
-  tags: string[];
-  statuses: string[];
-  search: string;
-}
-
-export type ViewMode = "list" | "kanban";
